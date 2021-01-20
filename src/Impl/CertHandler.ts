7e06f28da12e90b8d20bff3f0216c2a539ff6383
James Buncle <jbuncle@hotmail.com>
2021-01-20 00:00:12 +0000

    Added asynchronous cert generation (in case generate is re-triggered whilst in progress)

--- src/Impl/CertHandler.ts
+++ src/Impl/CertHandler.ts
@@ -8,6 +8,9 @@ import { format } from "util";
  * Handle creation of a certificate & key on filesystem.
  */
 export class CertHandler {
+
+    private readonly inProgressDomains: Record<string, boolean> = {};
+
     /**
       * 
       * @param certGenerator - CertGenerator that fetches the certificate from the certificate authority
@@ -26,19 +29,31 @@ export class CertHandler {
         // TODO: add async protection - to avoid 2 processes generating same for same domain
         // Check if certificate exists or needs renewal
         const certPath: string = this.getCertPath(commonName);
-        if (this.requiresRegeneration(certPath)) {
-            const result: CertResult = await this.certGenerator.generate({
-                commonName,
-            }, accountEmail);
+        if (this.requiresRegeneration(certPath) && !this.inProgress(commonName)) {
 
-            const keyPath: string = this.getKeyPath(commonName);
-            // Write cert
-            writeFileSync(certPath, result.certificate);
-            // Write key
-            writeFileSync(keyPath, result.privateKey);
+            this.inProgressDomains[commonName] = true;
+            try {
+                const result: CertResult = await this.certGenerator.generate({
+                    commonName,
+                }, accountEmail);
+
+                const keyPath: string = this.getKeyPath(commonName);
+                // Write cert
+                writeFileSync(certPath, result.certificate);
+                // Write key
+                writeFileSync(keyPath, result.privateKey);
+            } finally {
+                this.inProgressDomains[commonName] = false;
+            }
             return true;
         }
-        
+        return false;
+    }
+
+    private inProgress(commonName: string): boolean {
+        if (Object.prototype.hasOwnProperty.call(this.inProgressDomains, commonName) === true) {
+            return this.inProgressDomains[commonName];
+        }
         return false;
     }
 
