e3e955cf1abc5d3bfc1eb3ac7bdd2bc0518e1165
James Buncle <jbuncle@hotmail.com>
2021-01-22 16:46:59 +0000

    Improved WebRootChallengeHandler to check web root is an accessible path, create the challenge path and remove it afterward

--- src/ChallengeHandling/WebRootChallengeHandler.ts
+++ src/ChallengeHandling/WebRootChallengeHandler.ts
@@ -1,6 +1,7 @@
 import type { LoggerInterface } from "@jbuncle/logging-js";
 import type { Authorization, Challenge } from "acme-client/types/rfc8555";
-import { promises as fs } from 'fs';
+import { accessSync, constants, existsSync, mkdirSync, promises as fs, readdirSync, rmdirSync } from 'fs';
+import { join } from "path";
 import type { ChallengeHandlerI } from "../ChallengeHandlerI";
 
 /**
@@ -11,7 +12,13 @@ export class WebRootChallengeHandler implements ChallengeHandlerI {
     public constructor(
         private readonly logger: LoggerInterface,
         private readonly webRoot: string = `/var/www/html`,
-    ) { }
+    ) {
+        try {
+            accessSync(webRoot, constants.W_OK);
+        } catch (e: unknown) {
+            throw new Error(`Can't access '${webRoot}'`);
+        }
+    }
 
     public getTypes(): string[] {
         return [`http-01`];
@@ -19,7 +26,10 @@ export class WebRootChallengeHandler implements ChallengeHandlerI {
 
     public async create(authz: Authorization, challenge: Challenge, keyAuthorization: string): Promise<boolean> {
 
-        const filePath: string = `${this.getWebRoot()}/.well-known/acme-challenge/${challenge.token}`;
+        // Ensure acme-challenge directory exists
+        const parentPath: string = this.mkdirs(this.getWebRoot(), `.well-known`, `acme-challenge`);
+
+        const filePath: string = join(parentPath, challenge.token);
 
         this.logger.info(`Creating challenge response for ${authz.identifier.value} at path: ${filePath}`, {});
 
@@ -28,7 +38,6 @@ export class WebRootChallengeHandler implements ChallengeHandlerI {
 
         return true;
     }
-
 
     public async remove(authz: Authorization, challenge: Challenge): Promise<boolean> {
 
@@ -39,7 +48,56 @@ export class WebRootChallengeHandler implements ChallengeHandlerI {
         /* Replace this */
         this.logger.debug(`Removing "${filePath}"`, {});
         await fs.unlink(filePath);
+
+        this.rmdirs(this.getWebRoot(), `.well-known`, `acme-challenge`);
         return true;
+    }
+
+    /**
+     * Create each path in the parent path.
+     * @param parts 
+     */
+    private mkdirs(path: string, ...parts: string[]): string {
+        for (const part of parts) {
+            path = join(path, part);
+            this.mkdir(path);
+        }
+
+        return path;
+    }
+
+    private rmdirs(path: string, ...parts: string[]): void {
+        const paths: string[] = [];
+        for (const part of parts) {
+            paths.push(join(path, part));
+        }
+
+        const reversed: string[] = paths.reverse();
+        for (const fullPath of reversed) {
+            const removed: boolean = this.rmdir(fullPath);
+            if (!removed) {
+                // Can't remove any more if top level isn't empty
+                return;
+            }
+        }
+    }
+
+    private rmdir(dir: string): boolean {
+        if (existsSync(dir)) {
+            return true;
+        }
+        const files: string[] = readdirSync(dir);
+        if (files.length <= 0) {
+            rmdirSync(dir);
+            return true;
+        }
+        return false;
+    }
+
+    private mkdir(dir: string): void {
+        if (!existsSync(dir)) {
+            mkdirSync(dir);
+        }
     }
 
     private getWebRoot(): string {
